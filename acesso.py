112f3892bd2a85bfc3b48fab4a640aa319fbc45f
user.email <rogerio.dosantos@ifpr.edu.br>
2023-01-30 14:25:47 -0300

    Update acesso.py

--- acesso.py
+++ acesso.py
@@ -6,6 +6,7 @@ from earthaccess import Auth, Store, DataCollections, DataGranules
 auth = Auth()
 
 auth.login(strategy="interactive", persist=True)
+
 a = auth.login(strategy="environment")
 # are we authenticated?
 print(auth.authenticated)
